d9d3daf1a4abef5acef30062370c109a14b8e894
Hanno Lemoine <hanno.lemoine@ruhr-uni-bochum.de>
2011-07-18 17:15:13 +0200

    ERFOLG: TraceDroid printed LOGs
    Tracedroid build today printed Logs. I changed the function dexFileParse() in /dalvik/libdex/DexFile.c. DexFileParse() only controle the header. Next step is to look, wehre this function is called, and what happend after this.
    In Andrlid_dalvikVM_TypDefs.h I collect important TypeDefs, in order to print ist out. This ist not ready.

---
+++ Android_dalvikVM_TypDefs.h
@@ -0,0 +1,93 @@
+/*
+ * Author: Hanno Lemoine
+ * Version: 18.07.2011
+ * Summary: This Files contains some TypeDefs for the DalvikVM. Focused on the Dex-Parsing.
+ */
+
+//#########################
+//# dalvik/vm/Common.h - Z.60 - 72
+/*
+ * These match the definitions in the VM specification.
+ */
+#ifdef HAVE_STDINT_H
+# include <stdint.h>    /* C99 */
+typedef uint8_t             u1;
+typedef uint16_t            u2;
+typedef uint32_t            u4;
+typedef uint64_t            u8;
+typedef int8_t              s1;
+typedef int16_t             s2;
+typedef int32_t             s4;
+typedef int64_t             s8;
+
+
+
+
+
+
+//#########################
+//#dalvik/libdex/DexFile.h - Z.474 - 510
+/*
+ * Structure representing a DEX file.
+ *
+ * Code should regard DexFile as opaque, using the API calls provided here
+ * to access specific structures.
+ */
+typedef struct DexFile {
+    /* directly-mapped "opt" header */
+    const DexOptHeader* pOptHeader;
+
+    /* pointers to directly-mapped structs and arrays in base DEX */
+    const DexHeader*    pHeader;
+    const DexStringId*  pStringIds;
+    const DexTypeId*    pTypeIds;
+    const DexFieldId*   pFieldIds;
+    const DexMethodId*  pMethodIds;
+    const DexProtoId*   pProtoIds;
+    const DexClassDef*  pClassDefs;
+    const DexLink*      pLinkData;
+
+    /*
+     * These are mapped out of the "auxillary" section, and may not be
+     * included in the file.
+     */
+    const DexClassLookup* pClassLookup;
+    const void*         pRegisterMapPool;       // RegisterMapClassPool
+
+    /* points to start of DEX file data */
+    const u1*           baseAddr;
+
+    /* track memory overhead for auxillary structures */
+    int                 overhead;
+
+    /* additional app-specific data structures associated with the DEX */
+    //void*               auxData;
+} DexFile;
+
+//#########################
+//# dalvik/libdex/DexFile.h - Z.443 - 465
+/*
+ * Header added by DEX optimization pass.  Values are always written in
+ * local byte and structure padding.  The first field (magic + version)
+ * is guaranteed to be present and directly readable for all expected
+ * compiler configurations; the rest is version-dependent.
+ *
+ * Try to keep this simple and fixed-size.
+ */
+typedef struct DexOptHeader {
+    u1  magic[8];           /* includes version number */
+
+    u4  dexOffset;          /* file offset of DEX header */
+    u4  dexLength;
+    u4  depsOffset;         /* offset of optimized DEX dependency table */
+    u4  depsLength;
+    u4  optOffset;          /* file offset of optimized data tables */
+    u4  optLength;
+
+    u4  flags;              /* some info flags */
+    u4  checksum;           /* adler32 checksum covering deps/opt */
+
+    /* pad for 64-bit alignment if necessary */
+} DexOptHeader;
+
+
